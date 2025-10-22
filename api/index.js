1520e09483136c44cc3af696123cafb7c70841c2
moeez <moeezch095@gmail.com>
2025-10-22 19:12:09 +0500

    api => index.js ke file may change  kiya ha

--- api/index.js
+++ api/index.js
@@ -1,30 +1,37 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv");
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+// const dotenv = require("dotenv");
 
-const connectDB = require("../src/config/db");
-const userRoutes = require("../src/routes/userRoutes");
+// const connectDB = require("../src/config/db");
+// const userRoutes = require("../src/routes/userRoutes");
+// const app = require("../src/app");
+
+// dotenv.config();
+
+// const app = express();
+
+// // Middlewares
+// app.use(cors());
+// app.use(express.json());
+
+// // Database connection
+// connectDB();
+
+// // Routes
+// app.use("/api/users", userRoutes);
+
+// // Default route
+// app.get("/", (req, res) => {
+//   res.send("Hello from Vercel Serverless API!");
+// });
+
+// // Export for Vercel
+// module.exports = app;
+
+// api/index.js
+const serverless = require("serverless-http");
 const app = require("../src/app");
 
-dotenv.config();
+module.exports = serverless(app);
 
-const app = express();
-
-// Middlewares
-app.use(cors());
-app.use(express.json());
-
-// Database connection
-connectDB();
-
-// Routes
-app.use("/api/users", userRoutes);
-
-// Default route
-app.get("/", (req, res) => {
-  res.send("Hello from Vercel Serverless API!");
-});
-
-// Export for Vercel
-module.exports = app;
